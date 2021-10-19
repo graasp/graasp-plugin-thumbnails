5819ac5e7a7aa6e60a6c2d71cbef9cc24b8df681
Julien Torrent <Julien.torrent@outlook.com>
2021-10-19 10:52:50 +0200

    feat: add download sizes and save original

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
+import contentDisposition from 'content-disposition';
 import { FastifyPluginAsync } from 'fastify';
 import fastifyMultipart from 'fastify-multipart';
-import { mkdir } from 'fs/promises';
-import { IdParam } from 'graasp';
+import fs from 'fs';
+import { mkdir, unlink } from 'fs/promises';
+import { IdParam, UnknownExtra, Item } from 'graasp';
 import sharp from 'sharp';
 
-import { upload } from './schema';
+import { upload, download } from './schema';
 
+const ROUTES_PREFIX = '/thumbnails';
 const DEFAULT_MAX_FILE_SIZE = 1024 * 1024 * 250; // 250MB
+
+const randomHexOf4 = () =>
+  ((Math.random() * (1 << 16)) | 0).toString(16).padStart(4, '0');
 
 export interface GraaspFileItemOptions {
   /**
@@ -15,12 +21,14 @@ export interface GraaspFileItemOptions {
   storageRootPath: string;
 }
 
-const ROUTES_PREFIX = '/thumbnails';
-
 const plugin: FastifyPluginAsync<GraaspFileItemOptions> = async (
   fastify,
   options,
 ) => {
+  const {
+    items: { taskManager },
+    taskRunner: runner,
+  } = fastify;
   const { storageRootPath } = options;
 
   fastify.register(
@@ -40,27 +48,71 @@ const plugin: FastifyPluginAsync<GraaspFileItemOptions> = async (
         '/:id/upload',
         { schema: upload },
         async (request, reply) => {
-          //const { file, filename, mimetype, encoding } = await request.file();
           const data = await request.file();
           const { member, params: { id }, log } = request;
-          console.log(id);
+
+          const getItem = taskManager.createGetTaskSequence(member, id);
+          const item = (await runner.runSingleSequence(getItem, log)) as Item<UnknownExtra>;
+
+          const path = `${randomHexOf4()}/${randomHexOf4()}`;
 
           // validate edition of item and add thumbnail path
-          // ex: storageFilepath/randX4/randX4/
-
           // create directories path, 1 folder per item with all sizes
-          await mkdir(`${storageRootPath}/test1`, { recursive: true });
+          const storageFilepath = `${storageRootPath}/${path}`;
+          await mkdir(`${storageFilepath}`, { recursive: true });
 
-          // 'pump' file to directory
-          const storageFilepath = `${storageRootPath}/test1/1`;
-          //await pump(file, fs.createWriteStream(storageFilepath));
+          try {
+            // Save path to item and mimetype
+            const updateTask = taskManager.createUpdateTaskSequence(member, id, {
+              settings: {
+                ...item.settings,
+                thumbnail:{
+                  path: path,
+                  mimetype: data.mimetype
+                }
+              }
+            });
+            runner.runSingleSequence(updateTask, log);
 
-          // save resized images as with name corresponding to their sizes
-          const small = sharp(await data.toBuffer()).resize({ width: 200 }).toFile('output.png');
+            const imageBuffer = await data.toBuffer();
 
-          Promise.all([small]);
+            // save original and resized images as with name corresponding to their sizes
+            const original = sharp(imageBuffer).toFile(`${storageFilepath}/original`);
+            const small = sharp(imageBuffer).resize({ width: 200 }).toFile(`${storageFilepath}/small`);
+            const medium = sharp(imageBuffer).resize({ width: 400 }).toFile(`${storageFilepath}/medium`);
+            const large = sharp(imageBuffer).resize({ width: 600 }).toFile(`${storageFilepath}/large`);
+
+            Promise.all([small, medium, large, original]);
+          } catch (error) {
+            // unlink created files
+            const original = unlink(`${storageFilepath}/original`);
+            const small = unlink(`${storageFilepath}/small`);
+            const medium = unlink(`${storageFilepath}/medium`);
+            const large = unlink(`${storageFilepath}/large`);
+            Promise.all([small, medium, large, original]);
+
+            throw error;
+          }
 
           reply.send();
+        },
+      );
+
+      fastify.get<{ Params: IdParam, Querystring: { size: string } }>(
+        '/:id/download',
+        { schema: download },
+        async (request, reply) => {
+          const { member, params: { id }, query: { size }, log } = request;
+
+          const getItem = taskManager.createGetTaskSequence(member, id);
+          const { settings: { thumbnail: { path, mimetype }}} = await runner.runSingleSequence(getItem, log) as Item<UnknownExtra>;
+
+          // Get thumbnail path
+          reply.type(mimetype);
+          // this header will make the browser download the file with 'name' instead of
+          // simply opening it and showing it
+          reply.header('Content-Disposition', contentDisposition(`thumb-id-${size}`));
+          return fs.createReadStream(`${storageRootPath}/${path}/${size}`);
         },
       );
     },
--- src/schema.ts
+++ src/schema.ts
@@ -8,4 +8,23 @@ const upload = {
   },
 };
 
-export { upload };
+const download = {
+  params: { $ref: 'http://graasp.org/#/definitions/idParam' },
+  querystring: {
+    type: 'object',
+    properties:{ 
+      size:{
+        enum: [
+          'small',
+          'medium',
+          'large',
+          'original'
+        ],
+      }
+    },
+    required: ['size'],
+    additionalProperties: false,
+  }
+};
+
+export { upload, download };
