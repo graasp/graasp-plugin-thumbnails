41566660c3f8987918bfc0168d2602ad64dffa44
pyphilia <pyphilia@gmail.com>
2022-03-17 12:58:18 +0100

    refactor: update translations package

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -29,7 +29,7 @@ export class UploadFileNotImageError extends GraaspBaseError {
       {
         code: 'GPTERR001',
         statusCode: StatusCodes.BAD_REQUEST,
-        message: FAILURE_MESSAGES.FILE_NOT_IMAGE,
+        message: FAILURE_MESSAGES.FILE_IS_NOT_IMAGE,
       },
       data,
     );
